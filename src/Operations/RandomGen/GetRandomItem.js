cd1e0451999c3e6a9947019e73792bde50449b71
tmazyrko <tmazyrko@gmail.com>
2021-03-24 21:23:49 -0400

    function code

--- src/Operations/RandomGen/GetRandomItem.js
+++ src/Operations/RandomGen/GetRandomItem.js
@@ -1,4 +1,6 @@
-function GetRandomItem() {
-
+const sr = require("seedrandom");
+function GetRandomItem(list) {
+    var item = list[Math.floor(sr()*list.length)];
+    return item;
 }
 module.exports = GetRandomItem;
