422be826c702d15f95a6ae66de1f563b9067ccab
tmazyrko <tmazyrko@gmail.com>
2021-03-24 21:49:20 -0400

    added seeded GetRandomItem test

--- tests/RandomGen.test.js
+++ tests/RandomGen.test.js
@@ -6,3 +6,10 @@ test('GetRandomItem returns item from list', () => {
     console.log(result);
     expect(testList.includes(result)).toBe(true);
 });
+test('GetRandomItem, seeded', () => {
+    let testList = ["bink", "bank", "bonk"];
+    let result1 = GetRandomItem(testList, "testseed");
+    let result2 = GetRandomItem(testList, "testseed");
+    let result3 = GetRandomItem(testList, "testseed");
+    expect(result1==result2 && result2==result3).toBe(true);
+});
