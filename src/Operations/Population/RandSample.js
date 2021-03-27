324c78b6062a45a3ec5912616610dc26333084b3
tmazyrko <tmazyrko@gmail.com>
2021-03-27 19:36:23 -0400

    updated to pass args to SelectRandomly as Array

--- src/Operations/Population/RandSample.js
+++ src/Operations/Population/RandSample.js
@@ -3,6 +3,6 @@ const SelectRandomly = require('../RandomGen/SelectRandomly.js');
 function RandSample(c){
     let samplesize = c[0];
     let population = c[1].slice();
-    return SelectRandomly(samplesize, population);
+    return SelectRandomly([samplesize, population]);
 }
 module.exports = RandSample;
