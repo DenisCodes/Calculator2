fa8eca346d4cd390d3aa573a88f4a5a8ada9c600
tmazyrko <tmazyrko@gmail.com>
2021-03-27 23:13:21 -0400

    moved RandomGen unit tests into Calculator.test.js

--- tests/Calculator.test.js
+++ tests/Calculator.test.js
@@ -112,7 +112,64 @@ test('Calculator Zscore of a list', () => {
 });
 
 // Random Generator Functions
+test('GetRandomItem (unseeded) returns item from list', () => {
+    let testList = ["bink", "bank", "bonk"];
+    let result = Calculator.GetRandomItem(testList);
+    console.log(result);
+    expect(testList.includes(result)).toBe(true);
+});
 
+test('GetRandomItem (seeded) returns same item', () => {
+    let testList = ["bink", "bank", "bonk"];
+    let result1 = Calculator.GetRandomItem(testList, "testseed");
+    let result2 = Calculator.GetRandomItem(testList, "testseed");
+    let result3 = Calculator.GetRandomItem(testList, "testseed");
+    expect(result1==result2 && result2==result3).toBe(true);
+});
+
+test('SelectRandomly returns N number of items', () => {
+    let testList = [1, 2, 3, 4, 5, 6];
+    let result = Calculator.SelectRandomly(4, testList);
+    expect(result.length).toBe(4);
+});
+
+test('SelectRandomly (unseeded) returns a list', () => {
+    let testList = [1, 2, 3, 4, 5, 6];
+    let result = Calculator.SelectRandomly(3, testList);
+    console.log(result)
+    expect(result==undefined).toBe(false);
+});
+
+test('SelectRandomly (seeded) returns same lists', () => {
+    let testList = [1, 2, 3, 4, 5, 6];
+    let seed = "crumbcake";
+    let result1 = Calculator.SelectRandomly(3, testList, seed);
+    let result2 = Calculator.SelectRandomly(3, testList, seed);
+    console.log("Result 1: " + result1);
+    console.log("Result 2: " + result2);
+    expect(result1[0]==result2[0] && result1[1]==result2[1] && result1[2]==result2[2]).toBe(true);
+});
+
+test('GetRandomNumInRange (unseeded) returns a number within specified range', () => {
+    let min = 1, max = 50;
+    let result = Calculator.GetRandomNumInRange(min, max);
+    expect(result >= min && result <= max).toBe(true);
+});
+
+test('GetRandomNumInRange (seeded) returns same number', () => {
+    let min = 1, max = 50;
+    let seed = "hotel california";
+    let result1 = Calculator.GetRandomNumInRange(min, max, seed);
+    let result2 = Calculator.GetRandomNumInRange(min, max, seed);
+    expect(result1 == result2).toBe(true);
+});
+
+test('GenerateRandomList result contains correct amount of numbers', () => {
+    let amt = 5, min = 1, max = 50;
+    let seed = "hotel california";
+    let result = Calculator.GenerateRandomList(amt, min, max, seed);
+    expect(result.length).toBe(5);
+});
 
 // Population Statistics Functions
 test('Calculator Cochran of a list', () => {
