6a362fef62dae3fe4cfbb21741e85fcb8007b097
tmazyrko <tmazyrko@gmail.com>
2021-03-27 20:54:11 -0400

    added code to remove possible extra sample elements

--- src/Operations/Population/SystSample.js
+++ src/Operations/Population/SystSample.js
@@ -1,13 +1,18 @@
 function SystSample(c){
     let samplesize = c[0];
-    let population = c[1].splice();
+    let population = c[1].slice();
     let k = Math.round(population.length / samplesize);
 
     let sample = [];
 
-    for(let i = 0; i < samplesize; i += k)
+    for(let i = 0; i <= population.length; i += k)
     {
         sample.push(population[i]);
+    }
+
+    while(sample.length != samplesize)
+    {
+        sample = sample.slice(0, sample.length - 1);
     }
 
     return sample;
