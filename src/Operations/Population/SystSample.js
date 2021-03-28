8f4748f28639eed8952b2333c948395f3dc23412
tmazyrko <tmazyrko@gmail.com>
2021-03-27 20:30:14 -0400

    implement Systematic Sampling function

--- src/Operations/Population/SystSample.js
+++ src/Operations/Population/SystSample.js
@@ -1,4 +1,15 @@
 function SystSample(c){
+    let samplesize = c[0];
+    let population = c[1].splice();
+    let k = Math.round(population.length / samplesize);
 
+    let sample = [];
+
+    for(let i = 0; i < samplesize; i += k)
+    {
+        sample.push(population[i]);
+    }
+
+    return sample;
 }
 module.exports = SystSample;
